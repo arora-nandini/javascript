8a97ebac0a78f7718ac7f467463d2c26552722b5
arora-nandini <143823154+arora-nandini@users.noreply.github.com>
2024-08-23 14:05:55 +0000

    summary

--- .devcontainer/basics/conversionOperator.js
+++ .devcontainer/basics/conversionOperator.js
@@ -9,3 +9,22 @@ console.log(typeof value);
 let isLoggedin=1
 let booleanIsLoggedin=Boolean(isLoggedin)
 console.log(booleanIsLoggedin);
+
+///------operations------
+
+console.log(+true);
+//PRIMITIVE DATATYPE--string,number,null,undefined,boolean,symbol,BigInt
+//REFERENCE(NON PRIMITIVE)--arrays,objects,functions
+
+const heros=["shaktiman","doga"];//array
+let myObj={
+    name:"nandini",
+    age:19 ,    //objects
+}
+const myFunction=function(){
+    console.log("hello world");
+    }
+
+
+
+
