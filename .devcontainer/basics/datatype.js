a294bf8591bcc62164d1d7794d3899cc9f82bc5b
arora-nandini <143823154+arora-nandini@users.noreply.github.com>
2024-08-23 10:07:04 +0000

    variable and datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,9 @@
+"use strict";//treat code as new version
+let name="nandini"
+let age=18
+let isLoggedin=false //boolean
+
+//null-standalone value
+//symbol=unique
+
+console.log(typeof"nandini");
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,13 @@
+const accountId =1445
+let accountEmail="xyz@google.com"
+var accountPassword="123"
+accountCity="bhopal"
+let accountstate
+
+// accountId=2   // not allowed
+accountEmail="hc@hc.com"
+accountPassword="999"
+accountCity="indore"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountstate])
