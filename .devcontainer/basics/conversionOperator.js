5e1fddcf4b656d4ac8de83a43a5ad33eb93a1844
arora-nandini <143823154+arora-nandini@users.noreply.github.com>
2024-08-23 10:35:11 +0000

    conversions

---
+++ .devcontainer/basics/conversionOperator.js
@@ -0,0 +1,11 @@
+let score=22
+let value="22"
+console.log(typeof score);
+console.log(typeof value);
+let valueinNumber=Number(value)
+console.log(typeof value);
+//33abc gives NaN
+//true gives 1
+let isLoggedin=1
+let booleanIsLoggedin=Boolean(isLoggedin)
+console.log(booleanIsLoggedin);
