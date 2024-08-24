06cb6cd5af1d03944ea240bd44eeeaefb392d212
arora-nandini <143823154+arora-nandini@users.noreply.github.com>
2024-08-24 08:15:17 +0000

    math ,number ,string

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,21 @@
+const name="nandini"
+const repoCount=3
+console.log(`hello my name ${name} and my repo count is ${repoCount}`);
+
+const gameName= new String('hello world')
+console.log(gameName.charAt(0));
+console.log(gameName.slice(3,10));
+
+//----------------numbers and maths--------------
+const score =400
+const balance=new Number(100)
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+//----------MATH----------
+console.log(Math.random()); // gives value betwwen 0 and 1
+
+
+
+
+
+
