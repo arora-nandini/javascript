04bb52e4e9897ef417489e629cbd15538e9c19a2
arora-nandini <143823154+arora-nandini@users.noreply.github.com>
2024-08-24 17:58:54 +0000

    dateANDtime

---
+++ .devcontainer/basics/dateANDtime.js
@@ -0,0 +1,15 @@
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+let myCreatedDate =new Date(2024,0,27)
+console.log(myCreatedDate.toDateString);  //month start from zero in js
+
+
+
+
+
+
