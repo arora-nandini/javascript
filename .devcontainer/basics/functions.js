195f2606c742d51a54df32feef2152d5b36bfdd0
arora-nandini <143823154+arora-nandini@users.noreply.github.com>
2024-08-26 07:15:54 +0000

    functions

---
+++ .devcontainer/basics/functions.js
@@ -0,0 +1,34 @@
+function addtwonumbers (num1 ,num2){
+    console.log(num1+num2);
+    }
+    addtwonumbers(3,5)
+
+    function calculateCartPrice(val1,val2,...num3){//rest operator
+return num3
+    }
+    console.log(calculateCartPrice(200,400,600,900));
+    
+     const user={
+        name:"nandini",
+        age:19
+     }
+     function handleObject(anyObject){
+console.log(`username is ${anyObject.name} and age is ${anyObject.age}`);
+ }
+ handleObject(user)
+//scope
+//block,function and global scope
+
+function one(){
+    const username="nandini"
+    function two(){
+        const website="youtube"
+        console.log(username);
+        }
+        //console.log(website);
+        two()
+        }
+        one()
+ 
+
+
